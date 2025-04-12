5073ac99b8f06c52e19bfe3f25284a0b6a92ec7a
Aurelio de Padua <aurelio@Achachwaka1403.local>
2025-04-12 09:34:56 -0300

    Add ESLint v9 configuration

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      // Add any custom rules here
+    },
+  },
+]; 
